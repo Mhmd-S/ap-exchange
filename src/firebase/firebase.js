34fbaeb5c682cb9542baf2ac73c059fd197df361
Mhmd-S <65135074+Mhmd-S@users.noreply.github.com>
2023-06-17 12:34:00 +0800

    Some house keeping

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,17 +4,6 @@ import { getAuth } from "firebase/auth";
 import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
 import { connectStorageEmulator, getStorage } from "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAbQ4HTAo4eAx2R07r8ndxewqGEqWo6_do",
-  authDomain: "fir-1-2436b.firebaseapp.com",
-  projectId: "fir-1-2436b",
-  // storageBucket: "fir-1-2436b.appspot.com",
-  storageBucket: "127.0.0.1:8080",
-  messagingSenderId: "20000558993",
-  appId: "1:20000558993:web:0f2039d2546e86600f5430",
-  measurementId: "G-HJRHWERR0V"
-};
-
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 
